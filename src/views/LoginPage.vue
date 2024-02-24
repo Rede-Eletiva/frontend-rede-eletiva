32818b357bcfcb10d534b85640dd7aaecf997126
evellynamelia <evellynamelia2005@gmail.com>
2024-02-24 19:56:10 -0300

    LoginPage criado

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="container">
+        <Navbar />
+        <div id="content">
+          <MapComponet />
+          <div class="events-details">
+            <div class="grafic"></div>
+            <EventsComponent />
+          </div>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  </script>
+  
+  <style scoped>
+  </style>
+  
