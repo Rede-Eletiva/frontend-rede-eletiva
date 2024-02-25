1e46f82569eb9975f3a8a7a1326e65942a550c4a
evellynamelia <evellynamelia2005@gmail.com>
2024-02-25 14:04:28 -0300

    atualização

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,70 +1,44 @@
 <template>
     <div class="trace"></div>
-<div class="Tela-Principal">
-    <h1>Rede Eletiva</h1>
-    <div class="matricula">
-        <h2>matricula</h2>
-        <input 
-        type="number"
-        placeholder="1234567"
-      />
-    </div>
-    
-    <div class="Data-N">
-        <h2>Data Nascimento</h2>
-        <input 
-        type="date"
-        placeholder="01/02/2000"
-      />
-    </div>
+    <div class="Tela-Principal">
+        <h1>Rede Eletiva</h1>
+        <img class="photo" src="../assets/image 12.svg">
+        <img class="fita" src="../assets/Container right (1).svg">
+        <div class="matricula">
+            <h2>matricula</h2>
+            <input type="number" placeholder="1234567" />
+        </div>
 
-    <button class="bnt-login">
-    <span>Entrar</span>
-    </button>
-    <h3>Esqueci a Senha</h3>
-    </div>
+        <div class="Data-N">
+            <h2>Data Nascimento</h2>
+            <input type="date" placeholder="01/02/2000" />
+        </div>
 
+        <button class="bnt-login">
+            <span>Entrar</span>
+        </button>
+        <h3>Esqueci a Senha</h3>
+    </div>
 </template>
 
 <style scoped>
-.Tela-Principal h1 {
-    font-style: poppins;
-    
-
+.photo {
+    position: relative;
+    float: right;
+    margin-right: 50px;
+    left: 5%;
+    width: 40%;
 }
 
-.bnt-login {
-    width: 20%;
-    padding: 11px;
-    border-radius: 18px;
-    
-   
-  }
- 
-body {
-    
-    background-color: #EEF5FF;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
+.fita {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    right: 30%;
+    z-index: -10;
 }
 
 .Tela-Principal {
-
-    font-style: poppins;
-
-}
-
-.trace{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 10%;
-    height: 10%;
-    background-color: #ff8608;
-    clip-path: trace(50% at bottom right);
 }
 </style>
