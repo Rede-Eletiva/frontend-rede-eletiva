3184f626b9894aab9218fccf363851e3f0ee5335
evellynamelia <evellynamelia2005@gmail.com>
2024-02-24 23:31:22 -0300

    test

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="trace"></div>
+<div class="Tela-Principal">
+    <h1>Rede Eletiva</h1>
+    <div class="matricula">
+        <h2>matricula</h2>
+        <input 
+        type="number"
+        placeholder="1234567"
+      />
+    </div>
+    
+    <div class="Data-N">
+        <h2>Data Nascimento</h2>
+        <input 
+        type="date"
+        placeholder="01/02/2000"
+      />
+    </div>
+
+    <button class="bnt-login">
+    <span>Entrar</span>
+    </button>
+    <h3>Esqueci a Senha</h3>
+    </div>
+
+</template>
+
+<style scoped>
+.Tela-Principal h1 {
+    font-style: poppins;
+    
+
+}
+
+.bnt-login {
+    width: 20%;
+    padding: 11px;
+    border-radius: 18px;
+    
+   
+  }
+ 
+body {
+    
+    background-color: #EEF5FF;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.Tela-Principal {
+
+    font-style: poppins;
+
+}
+
+.trace{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 10%;
+    height: 10%;
+    background-color: #ff8608;
+    clip-path: trace(50% at bottom right);
+}
+</style>
--- src/views/MainScreen.vue
+++ src/views/MainScreen.vue
@@ -1,3 +1,93 @@
 <template>
-    <h1>Hello</h1>
-</template>
+    <h1 class="name">Rede Eletiva</h1>
+    <div class="select">
+        <h1>Selecione a turma:</h1>
+            <input
+            type="select"
+            name="id_adm"
+            placeholder="1 ANODS - A"
+          />  
+    </div>
+    <div class="container">
+        <div class="space1">
+            <h1>Eletiva 1 - Transformações Digitais e Novas Tecnologias</h1>
+            <h2>Professor: Juninho</h2>
+            <h3>4/45</h3>
+        </div>
+        <div class="space2">
+            <h1>Eletiva 2 - Consumo e Desenvolvimento no Mercado de Trabalho</h1>
+            <h2>Professora: Joaninha</h2>
+            <h3>10/45</h3>
+        </div>
+        <div class="space3">
+            <h1>Eletiva 3 - Sociologia e suas Novas Descobertas</h1>
+            <h2>Professora: Ana Clara</h2>
+            <h3>5/45</h3>
+        </div>
+        <div class="space4">
+            <h1>Eletiva 4 - Jogos de Quadra</h1>
+            <h2>Professor: Diego</h2>
+            <h3>39/45</h3>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  </script>
+  
+  <style scoped>
+
+  .name{
+    color: #2B6CB0;
+    font-family: 'Poppins', sans-serif;
+    font-size: 60px;
+    padding: 15px 30px;
+  }
+
+  .select {
+    background-color: blue;
+    font-family: 'Poppins';
+    padding: 15px 30px;
+    display: flex;
+  }
+  .container {
+    width: 99%;
+    padding: 15px 30px;
+  }
+
+  .space1{
+    background-color: #d9eafa;
+    width: 70%;
+    padding: 15px 30px;
+    color: #061b30;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  .space1 > h1 {
+    font-family: 'Poppins', sans-serif;
+    border: 3px solid black;
+    color: #3182CE;
+  }
+  .space2{
+    background-color: pink;
+    width: 70%;
+    opacity: 50%;
+    padding: 15px 30px;
+  }
+
+  .space3 {
+    background-color: blueviolet;
+    width: 70%;
+    opacity: 50%;
+    padding: 15px 30px;
+  }
+
+  .space4 {
+    background-color: chocolate;
+    width: 70%;
+    opacity: 50%;
+    padding: 15px 30px;
+  }
+
+  </style>
+  
