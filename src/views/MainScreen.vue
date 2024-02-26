bf8eedfc3462957397fff305180fbcd136e68968
dev-edsonlopes <dev_edson@outlook.com>
2024-02-25 21:57:59 -0300

    refatoring tela principal

--- src/views/MainScreen.vue
+++ src/views/MainScreen.vue
@@ -1,163 +1,177 @@
 <template>
-    <h1 class="name">Rede Eletiva</h1>
-    <svg class="fitas" width="1539" height="1080" viewBox="0 0 1539 1080" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <line opacity="0.49" y1="-13" x2="2217.85" y2="-13"
-            transform="matrix(-0.451533 0.892255 -0.994199 -0.107555 1558.03 -41.7339)" stroke="#FF0000"
-            stroke-width="26" />
-        <line opacity="0.49" y1="-300" x2="2217.85" y2="-300"
-            transform="matrix(-0.451533 0.892255 -0.994199 -0.107555 1579.31 -30.3713)" stroke="#0500FF"
-            stroke-width="600" />
-        <line opacity="0.49" y1="-13" x2="2217.77" y2="-13"
-            transform="matrix(-0.45145 0.892296 -0.994182 -0.107716 1497.15 -19.2368)" stroke="#24FF00" stroke-width="26" />
-        <line opacity="0.49" y1="-13" x2="2217.85" y2="-13"
-            transform="matrix(-0.451526 0.892258 -0.994197 -0.107569 1527.59 -30.3379)" stroke="#FFF500"
-            stroke-width="26" />
-    </svg>
-    <div class="select">
-        <h1>Selecione a turma:</h1>
-        <input type="select" name="id_adm" placeholder="1 ANODS - A" />
+  <div class="header">
+    <h1 class="title">Rede Eletiva</h1>
+
+    <div class="select-group">
+      <label for="select_classe">Selecionar Turma</label>
+      <select name="select_classe">
+        <option value=""></option>
+      </select>
     </div>
-    <div class="container">
-        <div class="space1">
-            <h1>Eletiva 1 - Transformações Digitais e Novas Tecnologias</h1>
-            <h2>Professor: Juninho</h2>
-            <h3>4/45</h3>
-            <button @click="handleButtonClick">Botão</button>
-        </div>
-        <div class="space2">
-            <h1>Eletiva 2 - Consumo e Desenvolvimento no Mercado de Trabalho</h1>
-            <h2>Professora: Joaninha</h2>
-            <h3>10/45</h3>
-            <button @click="handleButtonClick">Botão</button>
-        </div>
-        <div class="space3">
-            <h1>Eletiva 3 - Sociologia e suas Novas Descobertas</h1>
-            <h2>Professora: Ana Clara</h2>
-            <h3>5/45</h3>
-            <button @click="handleButtonClick">Botão</button>
-        </div>
-        <div class="space4">
-            <h1>Eletiva 4 - Jogos de Quadra</h1>
-            <h2>Professor: Diego</h2>
-            <h3>39/45</h3>
-            <button @click="handleButtonClick">Botão</button>
-        </div>
+  </div>
+
+  <div class="container">
+    <div class="content" v-for="elective in electives" :key="elective.key">
+      <div class="context">
+        <h1>{{ elective.code_elective }} - {{ elective.name_elective }}</h1>
+        <h2>Professor: Juninho</h2>
+      </div>
+
+      <div class="context-right">
+        <h3>{{ elective.filled_vacancies }}/ {{ elective.total_vacancies }}</h3>
+        <button type="submit">Selecionar</button>
+      </div>
     </div>
+    <button class="confirm-button" type="submit">Confirmar</button>
+  </div>
+  <img src="../assets/pernambucoMain.svg" class="pernambuco-main" />
 </template>
 
-<script></script>
+<script>
 
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export default {
+  data() {
+    return {
+      students: [],
+      electives: [],
+    };
+  },
+  mounted() {
+    this.fetchElectives();
+  },
+  methods: {
+    async fetchElectives() {
+      try {
+        const token = Cookies.get("_myapp_token");
+
+        const response = await axios.get(
+          "https://backend-rede-eletiva-ete.onrender.com/api/v1/discipline/list-electives",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        this.electives = response.data;
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+  },
+};
+</script>
 <style scoped>
-
-.fitas{
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 20%;
-    z-index: -10;
+.header {
+  width: 100vw;
+  height: 150px;
+  padding: 20px;
 }
-.name {
-    color: #2b6cb0;
-    font-family: "Poppins", sans-serif;
-    font-size: 65px;
-    padding: 5px 30px;
+.title {
+  color: #2b6cb0;
+  font-size: 65px;
 }
 
-.select {
-    font-family: "Poppins";
-    padding: 15px 30px;
-    display: flex;
+label {
+  padding-right: 20px;
+  font-weight: 700;
+  color: #737373;
+}
+select {
+  width: 100px;
+  height: 25px;
 }
 
 .container {
-    width: 94%;
-    margin: 0 auto;
-    padding: 15px 30px;
-    background-color: #F2FAFF;
-    border-radius: 25px;
+  margin-left: 20px;
+  width: 96vw;
+  min-height: 76vh;
+  max-height: 76vh;
+  background: #fff;
+  border: #7373736a 3px solid;
+  padding: 15px;
+  border-radius: 8px;
+  overflow-y: auto;
 }
 
-
-.space1 {
-    background-color: #d9eafa;
-    width: 80%;
-    min-height: auto;
-    padding: 15px 30px;
-    color: #061b30;
-    font-family: "Poppins", sans-serif;
-    margin-bottom: 25px;
-    border-radius: 25px;
-    text-align: left;
-    display: flex;
-    justify-content: column;
+.container .content {
+  background: #3182ce5e;
+  width: 80vw;
+  height: 150px;
+  border: 3px solid #73737382;
+  display: flex;
+  padding: 15px 8px;
+  justify-content: space-between;
+  border-radius: 8px;
+  padding: 20px;
+  margin-bottom: 15px;
 }
 
-.space1>h1,
-.space2>h1,
-.space3>h1,
-.space4>h1 {
-    font-family: "Poppins", sans-serif;
-    color: #3182ce;
+.content .context h1 {
+  font-size: 30px;
+  color: #3182ce;
+}
+.content .context h2 {
+  font-size: 20px;
+  color: #000000b1;
 }
 
-.space1>h2,
-.space2>h2,
-.space3>h2,
-.space4>h2 {
-    position: absolute;
-    top: 32%;
+.context-right {
+  height: 100%;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  justify-content: space-between;
 }
 
-.space1>h3,
-.space2>h3,
-.space3>h3,
-.space4>h3 {
-    margin-left: auto;
+button {
+  width: 150px;
+  height: 35px;
+  border-radius: 8px;
+  font-weight: 700;
+  background: #2b6cb0;
+  color: #fff;
+  border: 3px solid #00000063;
 }
 
-.space2 {
-    background-color: #3182ce62;
-    opacity: 60%;
-    width: 80%;
-    padding: 15px 30px;
-    margin-bottom: 25px;
-    border-radius: 25px;
-    font-family: "Poppins", sans-serif;
-    margin-bottom: 25px;
-    border-radius: 25px;
-    text-align: left;
-    display: flex;
-    justify-content: column;
+.confirm-button {
+  position: fixed;
+  bottom: 5%;
+  right: 5%;
+  background-color: #3d7e3376;
+  cursor: pointer;
+  transition: 0.3s;
 }
 
-.space3 {
-    background-color: #3182ce62;
-    width: 80%;
-    opacity: 50%;
-    padding: 15px 30px;
-    margin-bottom: 25px;
-    border-radius: 25px;
-    font-family: "Poppins", sans-serif;
-    margin-bottom: 25px;
-    border-radius: 25px;
-    text-align: left;
-    display: flex;
-    justify-content: column;
+.confirm-button:hover {
+  background-color: #3d7e33d0;
 }
 
-.space4 {
-    background-color: #3182ce62;
-    width: 80%;
-    opacity: 50%;
-    padding: 15px 30px;
-    margin-bottom: 25px;
-    font-family: "Poppins", sans-serif;
-    margin-bottom: 25px;
-    border-radius: 25px;
-    text-align: left;
-    display: flex;
-    justify-content: column;
+.pernambuco-main {
+  position: fixed;
+  bottom: 0;
+  right: -15%;
+  z-index: -1;
+}
 
+.container::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+  background-color: #cccccc;
+}
+
+.container::-webkit-scrollbar-track {
+  background: #cccccc;
+}
+.container::-webkit-scrollbar-track-piece {
+  background: #cccccc;
+}
+.container::-webkit-scrollbar-thumb {
+  background: #2b6cb0;
+  border-radius: 5px;
 }
 </style>
